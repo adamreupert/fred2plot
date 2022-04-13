6706a84fad4daf7e7f1faa84436eda5c751568e3
Adam Reupert <adam.reupert@web.de>
2022-04-13 17:19:36 +0200

    created working 2 layer scripts of tkui and XRD_tkui

--- Fred_tkui_XRD.py
+++ Fred_tkui_XRD.py
@@ -11,24 +11,32 @@ from tkinter import Tk, Button, Radiobutton, Grid, N, S, W, E, Frame, IntVar, La
 
 
 def menus(mainwindow):
-    print('Yippey')
+    
+    def xrd_plot():
+        XRD_plotmenu_frame.grid()
+
+    
+    # Creation of subgrids
     XRD_submethods_frame=Frame(mainwindow)
     XRD_submethods_frame.grid(row=2, column=0, columnspan=3) # Columnspan has to be the same number as there are submenus
     XRD_plotmenu_frame=Frame(mainwindow)
     XRD_plotmenu_frame.grid(row=4, column=0, columnspan=7) # Columnspan has to be the same number as there are in total
-    
+
+    #Creation of first subgrid (Plot,Rietveld,...) & hide when first initialized
     Button_XRD_rietveld = Button(XRD_submethods_frame, text='Rietveld', width=25, height = 1, state = "normal")
     Button_XRD_rietveld.grid(row=0,column=1,sticky=N+S)
     Button_XRD_pdf = Button(XRD_submethods_frame, text='PDF', width=25, height = 1, state = "normal")
     Button_XRD_pdf.grid(row=0,column=2,sticky=N+S)
+    Button_XRD_plot = Button(XRD_submethods_frame, text='Plot', width=25, height = 1, state = "normal", command = xrd_plot)
+    Button_XRD_plot.grid(row=0,column=0,sticky=N+S)
     XRD_submethods_frame.grid_remove()
     
-    
-    #plotmenu
-    Button_XRD5 = Button(XRD_plotmenu_frame, text='Plot', width=25, height = 1, state = "normal")
-    Button_XRD5.grid(row=0,column=0,sticky=N+S)
-    Button_XRD_plot = Button(XRD_submethods_frame, text='Plot', width=25, height = 1, state = "normal", command = XRD_plotmenu_frame.grid())
-    Button_XRD_plot.grid(row=0,column=0,sticky=N+S)
+    #Creation of Plot widget & hide when first initialized
+    Button_XRD_plot_widget = Button(XRD_plotmenu_frame, text='Generate', width=25, height = 1, state = "normal")
+    Button_XRD_plot_widget.grid(row=0,column=0,sticky=N+S)
     XRD_plotmenu_frame.grid_remove()
+    
+    # Return of first subgrid to be callable from Fred_tkui class
     return XRD_submethods_frame
+    
     
